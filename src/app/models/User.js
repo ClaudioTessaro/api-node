5669297dea0be263c625790d32a9dbd71fb1bffa
Claudio Aragao Tessaro <claudio.tessaro@ctis.com.br>
2020-03-09 17:30:44 -0300

    Criando controllers, middlewares e autenticação do sistema

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from "sequelize";
+import bycrypt from "bcryptjs";
 
 class User extends Model {
     static init(sequelize) {
@@ -7,12 +8,27 @@ class User extends Model {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
                 password_hash: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
                 provider: Sequelize.BOOLEAN,
             },
             {
                 sequelize,
             }
         );
+        // o hook são trechos de codigo que
+        // são executados de forma automatica, baseado em ações que acontecem no
+        // nosso model
+        this.addHook("beforeSave", async user => {
+            if (user.password) {
+                user.password_hash = await bycrypt.hash(user.password, 8);
+            }
+        });
+
+        return this;
+    }
+
+    checkPassword(password) {
+        return bycrypt.compare(password, this.password_hash);
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 
-import User from "./app/models/User";
+import UserController from "./app/controllers/UserController";
+import SessionController from "./app/controllers/SessionController";
+import authMiddleware from "./app/middlewares/auth";
 
 const routes = new Router();
 
-routes.get("/", async (req, res) => {
-    const user = await User.create({
-        name: "Claudio Tessaro",
-        email: "claudio@01906",
-        password_hash: "12345",
-    });
-    return res.json(user);
-});
+routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
+
+// As rotas que vierem após esse middleware precisarão ser autenticada
+routes.use(authMiddleware);
+routes.put("/users", UserController.update);
 
 export default routes;
