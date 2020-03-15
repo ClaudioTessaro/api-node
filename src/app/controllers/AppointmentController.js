307d429f31dfbb359b24ee1c92a7d2ba90176e43
Claudio Tessaro <claudio0190@hotmail.com>
2020-03-15 17:20:44 -0300

    Inserindo paginação

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,10 +6,13 @@ import Appointments from "../models/Appointments";
 
 class AppointmentController {
     async index(req, res) {
+        const { page = 1 } = req.query;
         const appointments = await Appointments.findAll({
             where: { user_id: req.userId, canceled_at: null },
             order: ["date"],
             attributes: ["id", "date"],
+            limit: 20,
+            offset: (page - 1) * 20,
             include: [
                 {
                     model: User,
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import AppointmentController from "./app/controllers/AppointmentController";
 import authMiddleware from "./app/middlewares/auth";
+import ScheduleController from "./app/controllers/ScheduleController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -25,4 +26,5 @@ routes.post("/files", upload.single("file"), FileController.store);
 routes.get("/providers", ProviderController.index);
 routes.post("/appointments", AppointmentController.store);
 routes.get("/appointments", AppointmentController.index);
+routes.get("/schedules", ScheduleController.index);
 export default routes;
