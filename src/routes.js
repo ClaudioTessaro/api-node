5672fbae8bd7bbf00d74172ba1d0353549208f36
Claudio Tessaro <claudio0190@hotmail.com>
2020-03-20 12:18:45 -0300

    Adicionando a notificação para o usuario

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from "./app/controllers/SessionController";
 import AppointmentController from "./app/controllers/AppointmentController";
 import authMiddleware from "./app/middlewares/auth";
 import ScheduleController from "./app/controllers/ScheduleController";
+import NotificationController from "./app/controllers/NotificationController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,5 +24,9 @@ routes.post("/files", upload.single("file"), FileController.store);
 routes.get("/providers", ProviderController.index);
 routes.post("/appointments", AppointmentController.store);
 routes.get("/appointments", AppointmentController.index);
+routes.delete("/appointments/:id", AppointmentController.delete);
 routes.get("/schedules", ScheduleController.index);
+routes.get("/notifications", NotificationController.index);
+routes.put("/notifications/:id", NotificationController.update);
+
 export default routes;
