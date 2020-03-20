7d28e841dd43c1594811e535dbfa9a3e7db7f50a
Claudio Tessaro <claudio0190@hotmail.com>
2020-03-20 19:39:26 -0300

    Configurando envio de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    secure: false,
+    auth: {
+        user: "94e2c22c31fa5b",
+        pass: "48889d64023a2a",
+    },
+    default: {
+        from: "Equipe GoBarber <noreply@gobarber.com",
+    },
+};
