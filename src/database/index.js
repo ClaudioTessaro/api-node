d0319c6c03eb10811470a959be325d676f8b8070
Claudio Tessaro <claudio0190@hotmail.com>
2020-03-22 15:06:37 -0300

    Corrigindo url do mongo

--- src/database/index.js
+++ src/database/index.js
@@ -30,6 +30,7 @@ class Database {
     mongo() {
         this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
+
             useFindAndModify: true,
         });
     }
